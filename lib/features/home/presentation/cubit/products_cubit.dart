9c6ca440005cd75d42e409939a17d0be81a3360b
mustafasaeed23 <mustafasaeed5580@gmail.com>
2025-07-21 20:08:34 +0300

    update

--- lib/features/home/presentation/cubit/products_cubit.dart
+++ lib/features/home/presentation/cubit/products_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:store_app/features/home/domain/entities/product_entity.dart';
 import 'package:store_app/features/home/domain/usecases/products_use_case.dart';
 import 'package:store_app/features/home/presentation/cubit/products_state.dart';
 
@@ -7,13 +8,40 @@ class ProductsCubit extends Cubit<ProductsState> {
   ProductsCubit({required this.productsUseCase}) : super(ProductsInitial());
 
   final ProductsUseCase productsUseCase;
+  List<ProductEntity> _originalList = [];
 
   Future<void> getAllProducts() async {
     emit(ProductsLoading());
     final result = await productsUseCase.call();
-    result.fold((l) {
-      emit(ProductsError(message: l.message));
-      print(l.message);
-    }, (r) => emit(ProductsLoaded(productsList: r)));
+    result.fold(
+      (l) {
+        emit(ProductsError(message: l.message));
+      },
+      (r) {
+        _originalList = r;
+        emit(ProductsLoaded(productsList: r));
+      },
+    );
+  }
+
+  void filterByCategory(String category) {
+    final filtered = _originalList.where((product) => product.category == category).toList();
+    emit(ProductsLoaded(productsList: filtered));
+  }
+
+  void sortByPriceAscending() {
+    final currentList = List<ProductEntity>.from(_originalList);
+    currentList.sort((a, b) => a.price.compareTo(b.price));
+    emit(ProductsLoaded(productsList: currentList));
+  }
+
+  void sortByPriceDescending() {
+    final currentList = List<ProductEntity>.from(_originalList);
+    currentList.sort((a, b) => b.price.compareTo(a.price));
+    emit(ProductsLoaded(productsList: currentList));
+  }
+
+  void resetFilters() {
+    emit(ProductsLoaded(productsList: _originalList));
   }
 }
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/helpers/extensions.dart';
 import 'package:store_app/core/routing/routes.dart';
 import 'package:store_app/core/theming/colors.dart';
 import 'package:store_app/core/widgets/custom_drop_down.dart';
 import 'package:store_app/core/widgets/custom_texts.dart';
+import 'package:store_app/features/home/presentation/cubit/products_cubit.dart';
+import 'package:store_app/features/home/presentation/cubit/products_state.dart';
 import 'package:store_app/features/home/presentation/widgets/products_list_widget.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -30,6 +33,13 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     currentUser = FirebaseAuth.instance.currentUser;
     isGuest = currentUser == null || currentUser!.isAnonymous;
+
+    // Load products initially
+    context.read<ProductsCubit>().getAllProducts();
+  }
+
+  void _onFilterOrSortChanged() {
+    context.read<ProductsCubit>().getAllProducts();
   }
 
   @override
@@ -81,6 +91,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       setState(() {
                         selectedFilter = value;
                       });
+                      _onFilterOrSortChanged();
                     },
                   ),
                 ),
@@ -95,13 +106,27 @@ class _HomeScreenState extends State<HomeScreen> {
                       setState(() {
                         selectedSorting = value;
                       });
+                      _onFilterOrSortChanged();
                     },
                   ),
                 ),
               ],
             ),
             SizedBox(height: 20.h),
-            Expanded(child: SingleChildScrollView(child: ProductsListWidget())),
+            Expanded(
+              child: BlocBuilder<ProductsCubit, ProductsState>(
+                builder: (context, state) {
+                  if (state is ProductsLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is ProductsError) {
+                    return Center(child: Text(state.message));
+                  } else if (state is ProductsLoaded) {
+                    return ProductsListWidget();
+                  }
+                  return const SizedBox.shrink();
+                },
+              ),
+            ),
           ],
         ),
       ),
